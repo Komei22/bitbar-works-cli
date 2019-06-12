6e2051c09f447a4957d8090fb9f6dfe6b9dd0152
Komei Nomura <komei.nomura@pepabo.com>
2019-06-12 17:08:43 +0900

    add loggingWorkHistory function

--- atendance/atendance.go
+++ atendance/atendance.go
@@ -1,24 +1,36 @@
 package atendance
 
-import(
-	"os"
-
-	"net/http"
-	"net/url"
+import (
+	"fmt"
+	"os/exec"
+	"time"
 )
 
-func PostEndWork() error {
-	postURL := "http://compweb01.gmo.local/cws/srwtimerec"
-	values := url.Values{}
-	values.Add("dakoku", "taisya")
-	values.Add("user_id", os.Getenv("WORK_USER"))
-	values.Add("password", os.Getenv("WORK_PASSWORD"))
+// EndWork post and record end work time
+func EndWork() error {
+	// postURL := "http://compweb01.gmo.local/cws/srwtimerec"
+	// values := url.Values{}
+	// values.Add("dakoku", "taisya")
+	// values.Add("user_id", os.Getenv("WORK_USER"))
+	// values.Add("password", os.Getenv("WORK_PASSWORD"))
 
-	_, err := http.PostForm(postURL, values)
+	// _, err := http.PostForm(postURL, values)
+	// if err != nil {
+	// 	return err
+	// }
+	err := loggingWorkHistory("end")
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-
+func loggingWorkHistory(action string) error {
+	t := time.Now().Format(time.RFC3339)
+	cmdstr := fmt.Sprintf(`echo '%s, %s' >> ~/.work_history`, action, t)
+	_, err := exec.Command("sh", "-c", cmdstr).Output()
+	if err != nil {
+		return err
+	}
+	return nil
+}
