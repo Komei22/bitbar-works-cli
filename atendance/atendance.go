dd57c8ef7dba44e2bbc4c3cec4a20d63ad6eaa06
Komei Nomura <komei.nomura@pepabo.com>
2019-06-12 19:49:38 +0900

    refactoring atendance

--- atendance/atendance.go
+++ atendance/atendance.go
@@ -2,51 +2,49 @@ package atendance
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
+	"os"
 	"os/exec"
 	"time"
 )
 
-// StartWork post and record starting work time
-func StartWork() error {
-	// postURL := "http://compweb01.gmo.local/cws/srwtimerec"
-	// values := url.Values{}
-	// values.Add("dakoku", "syussya")
-	// values.Add("user_id", os.Getenv("WORK_USER"))
-	// values.Add("password", os.Getenv("WORK_PASSWORD"))
+// Action is atendance action
+type Action int
 
-	// _, err := http.PostForm(postURL, values)
-	// if err != nil {
-	// 	return err
-	// }
-	err := loggingWorkHistory("start")
+const (
+	// StartWork is starting work action
+	StartWork = iota
+	// EndWork is ending work action
+	EndWork
+)
+
+// StampAtendance post and record atendance information.
+func StampAtendance(action Action) (time.Time, error) {
+	t := time.Now()
+	loggingWorkHistory(action, t)
+	return t, nil
+}
+
+func postAtendance(action Action) error {
+	postURL := "http://compweb01.gmo.local/cws/srwtimerec"
+	values := url.Values{}
+	values.Add("user_id", os.Getenv("WORK_USER"))
+	values.Add("password", os.Getenv("WORK_PASSWORD"))
+	if action == StartWork {
+		values.Add("dakoku", "syussya")
+	} else {
+		values.Add("dakoku", "taisya")
+	}
+	_, err := http.PostForm(postURL, values)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-// EndWork post and record ending work time
-func EndWork() error {
-	// postURL := "http://compweb01.gmo.local/cws/srwtimerec"
-	// values := url.Values{}
-	// values.Add("dakoku", "taisya")
-	// values.Add("user_id", os.Getenv("WORK_USER"))
-	// values.Add("password", os.Getenv("WORK_PASSWORD"))
-
-	// _, err := http.PostForm(postURL, values)
-	// if err != nil {
-	// 	return err
-	// }
-	err := loggingWorkHistory("end")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func loggingWorkHistory(action string) error {
-	t := time.Now().Format(time.RFC3339)
-	cmdstr := fmt.Sprintf(`echo '%s, %s' >> ~/.work_history`, action, t)
+func loggingWorkHistory(action Action, t time.Time) error {
+	cmdstr := fmt.Sprintf(`echo '%d, %s' >> ~/.work_history`, action, t.Format(time.RFC3339))
 	_, err := exec.Command("sh", "-c", cmdstr).Output()
 	if err != nil {
 		return err
