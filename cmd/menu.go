38694328c1ecd218bae4369ef5f45d5a2e5f7143
Komei Nomura <komei.nomura@pepabo.com>
2019-06-24 12:33:43 +0900

    change the display strings according to work status
    - workstatus(beforeWork, working, afterWork)

--- cmd/menu.go
+++ cmd/menu.go
@@ -1,10 +1,19 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/Komei22/bitbar-works-go/atendance"
 	"github.com/johnmccabe/go-bitbar"
 	"github.com/spf13/cobra"
+)
+
+const (
+	beforeWork = iota
+	working
+	afterWork
 )
 
 // menuCmd represents the menu command
@@ -15,10 +24,32 @@ var menuCmd = &cobra.Command{
 		ex, _ := os.Executable()
 
 		plugin := bitbar.New()
-		plugin.StatusLine("🏢🚶‍")
 		submenu := plugin.NewSubMenu()
-		submenu.Line("Start work").Bash(ex).Params([]string{"start"}).Terminal(false)
-		submenu.Line("Finish work").Bash(ex).Params([]string{"finish"}).Terminal(false)
+
+		startStatus := "Not yet"
+		finishStatus := "Not yet"
+
+		a := atendance.Atendance{}
+		a.SetAtendanceInfo()
+
+		switch checkWorkStatus(a) {
+		case beforeWork:
+			plugin.StatusLine(fmt.Sprintf("🏢 %s /🚶‍ %s", startStatus, finishStatus)).Color("red")
+			submenu.Line("Start work").Bash(ex).Params([]string{"start"}).Terminal(false)
+			submenu.Line("Finish work").Bash(ex).Params([]string{"finish"}).Terminal(false)
+		case working:
+			startStatus := a.SwTime.Format("15:04")
+			plugin.StatusLine(fmt.Sprintf("🏢 %s /🚶‍ %s", startStatus, finishStatus))
+			submenu.Line("Start work")
+			submenu.Line("Finish work").Bash(ex).Params([]string{"finish"}).Terminal(false)
+		case afterWork:
+			startStatus := a.SwTime.Format("15:04")
+			finishStatus := a.FwTime.Format("15:04")
+			plugin.StatusLine(fmt.Sprintf("🏢 %s /🚶‍ %s", startStatus, finishStatus))
+			submenu.Line("Start work")
+			submenu.Line("Finish work")
+		default:
+		}
 
 		plugin.Render()
 	},
@@ -26,4 +57,28 @@ var menuCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(menuCmd)
+}
+
+func isStampAtendance(t time.Time) bool {
+	if !isToday(t) || t.IsZero() {
+		return false
+	}
+	return true
+}
+
+func isToday(t time.Time) bool {
+	if t.Day() == time.Now().Day() {
+		return true
+	}
+	return false
+}
+
+func checkWorkStatus(a atendance.Atendance) int {
+	if !isStampAtendance(a.SwTime) && !isStampAtendance(a.FwTime) {
+		return beforeWork
+	} else if isStampAtendance(a.SwTime) && !isStampAtendance(a.FwTime) {
+		return working
+	} else {
+		return afterWork
+	}
 }
